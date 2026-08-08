68e6ce9026dd17939c8899ffae73c0a9e25ef8a9
9osiris <9osiris@users.noreply.github.com>
2026-08-08 13:42:22 -0400

    Set default site accent color to White (#ffffff)

--- orc-script.js
+++ orc-script.js
@@ -7,7 +7,7 @@ const IMG_W45   = "https://image.tmdb.org/t/p/w45";
 const IMG_ORIG  = "https://image.tmdb.org/t/p/original";
 const IMG_W780  = "https://image.tmdb.org/t/p/w780";
 const BRAND     = "Osiris Watch";
-const ACCENT    = "5b99fc";
+const ACCENT     = "ffffff";
 
 const PROVIDERS = [
   { id: "vidking", name: "VidKing",
